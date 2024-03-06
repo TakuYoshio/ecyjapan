d56189a5c8202621bbd4642f11ab346a1c3ecd7a
KaimuSeino <skim07a19yki@icloud.com>
2024-03-06 15:35:23 +0900

    仮の完成

---
+++ script.js
@@ -0,0 +1,72 @@
+// メイン画像
+const img = [
+  "/image/luna-kay-tCapTaW1qpo-unsplash.jpg",
+  "/image/david-edelstein-N4DbvTUDikw-unsplash.jpg",
+  "/image/jezael-melgoza-alY6_OpdwRQ-unsplash.jpg",
+]
+
+let imageCount = -1;
+picChange();
+function picChange() {
+  imageCount++;
+  if (imageCount == img.length) imageCount = 0;
+  //画像選択
+  changePic.src = img[imageCount];
+  //秒数の指定
+  setTimeout("picChange()", 4000);
+}
+
+
+// スライドショー
+const slide = document.getElementById('slide');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const indicator = document.getElementById('indicator');
+const lists = document.querySelectorAll('.list');
+const totalSlides = lists.length;
+let count = 0;
+let autoPlayInterval;
+function updateListBackground() {
+  for (let i = 0; i < lists.length; i++) {
+    lists[i].style.backgroundColor = i === count % totalSlides ? '#000' : '#fff';
+  }
+}
+function nextClick() {
+  slide.classList.remove(`slide${count % totalSlides + 1}`);
+  count++;
+  slide.classList.add(`slide${count % totalSlides + 1}`);
+  updateListBackground();
+}
+function prevClick() {
+  slide.classList.remove(`slide${count % totalSlides + 1}`);
+  count--;
+  if (count < 0) count = totalSlides - 1;
+  slide.classList.add(`slide${count % totalSlides + 1}`);
+  updateListBackground();
+}
+function startAutoPlay() {
+  autoPlayInterval = setInterval(nextClick, 5000);
+}
+function resetAutoPlayInterval() {
+  clearInterval(autoPlayInterval);
+  startAutoPlay();
+}
+next.addEventListener('click', () => {
+  nextClick();
+  resetAutoPlayInterval();
+});
+prev.addEventListener('click', () => {
+  prevClick();
+  resetAutoPlayInterval();
+});
+indicator.addEventListener('click', (event) => {
+  if (event.target.classList.contains('list')) {
+    const index = Array.from(lists).indexOf(event.target);
+    slide.classList.remove(`slide${count % totalSlides + 1}`);
+    count = index;
+    slide.classList.add(`slide${count % totalSlides + 1}`);
+    updateListBackground();
+    resetAutoPlayInterval();
+  }
+});
+// startAutoPlay();
